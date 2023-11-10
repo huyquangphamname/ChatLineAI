f28e939116575408c8371f0d014c911688bc94fe
Quang Huy <quanghuy97lqd@gmail.com>
2023-11-10 16:58:50 +0800

    add the suggest code line to the reportWebVitals

--- projectReact-chatgpt-clone/react-chatgpt-clone/src/index.js
+++ projectReact-chatgpt-clone/react-chatgpt-clone/src/index.js
@@ -2,7 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+//import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// reportWebVitals();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
