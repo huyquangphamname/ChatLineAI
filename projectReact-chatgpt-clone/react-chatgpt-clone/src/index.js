c5ee5e550c0634542beb84015961d77874150184
Quang Huy <quanghuy97lqd@gmail.com>
2023-11-10 17:06:33 +0800

    Reverse changes

--- projectReact-chatgpt-clone/react-chatgpt-clone/src/index.js
+++ projectReact-chatgpt-clone/react-chatgpt-clone/src/index.js
@@ -3,16 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 //import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// reportWebVitals();
-
+//import { StrictMode } from 'react';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
