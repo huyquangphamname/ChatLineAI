2f3890ba8224cf888ea60b611c8b28506653b3f3
Quang Huy <quanghuy97lqd@gmail.com>
2023-11-11 15:15:08 +0800

    Add class getMessages

--- projectReact-chatgpt-clone/react-chatgpt-clone/src/App.js
+++ projectReact-chatgpt-clone/react-chatgpt-clone/src/App.js
@@ -1,4 +1,26 @@
 const App = () => {
+  const getMessages = async () => {
+    const options ={
+      method: "POST",
+      body: JSON.stringify({
+        message: "Hello, how are you?"
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    }
+    try {
+      const response = await fetch('http://localhost:8000/completions', options)
+      const data = await response.json()
+      console.log(data)
+    } catch (error) {
+        console.error(error)
+    }
+
+
+  }
+
+
   return (
     <div className="app">
       <section className="side-bar">
@@ -19,7 +41,7 @@ const App = () => {
         <div className="bottom-section">
           <div className="input-container">
             <input/>
-            <div id="submit">➢</div>
+            <div id="submit" onClick={getMessages}>➢</div>
           </div>
           <p className="info">
             ChatGPT is a chatbot that uses the GPT-3 API to generate responses to your messages.
