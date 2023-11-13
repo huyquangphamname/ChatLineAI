93ec5ae67b7670d06a101931534d639324fedaed
Quang Huy <quanghuy97lqd@gmail.com>
2023-11-13 22:43:36 +0800

    To format the correct form for the <input> element with the value and onChange attributes Resolve the issue and have a properly formatted <input> element.

--- projectReact-chatgpt-clone/react-chatgpt-clone/src/App.js
+++ projectReact-chatgpt-clone/react-chatgpt-clone/src/App.js
@@ -1,10 +1,23 @@
+import { useState, useEffect} from 'react'
 const App = () => {
-  const [ value, setValue ] = useState(null)
+  const [ value, setValue ] = useState("")
+  const [ message, setMessage ] = useState(null)
+  const [ previousMessages, setPreviousMessages ] = useState([])
+  const [ currentTitle, setCurrentTitle ] = useState([])
+  /**
+   * Fetches messages from the server.
+   * 
+   * This function sends a POST request to the server with the user's message.
+   * The server processes the message and returns a response.
+   * The response is then set as the messages state.
+   * 
+   * @returns {Promise<void>} A promise that resolves when the messages are fetched.
+   */
   const getMessages = async () => {
-    const options ={
+    const options = {
       method: "POST",
       body: JSON.stringify({
-        message: "Hello, how are you?"
+        message: value
       }),
       headers: {
         "Content-Type": "application/json",
@@ -14,6 +27,7 @@ const App = () => {
       const response = await fetch('http://localhost:8000/completions', options)
       const data = await response.json()
       console.log(data)
+      setMessage(data.choices[0].message)
     } catch (error) {
         console.error(error)
     }
@@ -21,7 +35,29 @@ const App = () => {
 
   }
 
-  console.log(value)
+  useEffect(() => {
+    console.log(currentTitle, value, message)
+    if (!currentTitle && value && message) {
+      setCurrentTitle(value)
+    }
+    if (currentTitle && value && message) {
+      setPreviousMessages(prevChats => (
+        [...prevChats, {
+            title: currentTitle,
+            role: "user",
+            content: value
+          },
+          {
+            title: currentTitle,
+            role: message.role,
+            content: message.content
+          }
+        ]
+      ))
+    }
+  }, [message, currentTitle])
+
+    console.log(previousMessages)
 
   return (
     <div className="app">
@@ -32,17 +68,24 @@ const App = () => {
         </ul>
         <nav>
           <p>Make by App</p>
-          </nav>
+        </nav>
       </section>
         
       <section className="main">
-        <h1>AniaGPT</h1>
+        {!currentTitle && <h1>AniaGPT</h1>}
         <ul className="feed">
           
         </ul>
         <div className="bottom-section">
           <div className="input-container">
-            <input value = {value} onChange = {(e) => setValue(e.target.value)}/>
+            <label htmlFor="myInput">Input:</label>
+            <input
+              type="text"
+              id="myInput"
+              name="myInput"
+              value = {value} 
+              onChange = {(e) => setValue(e.target.value)}
+            />
             <div id="submit" onClick={getMessages}>➢</div>
           </div>
           <p className="info">
